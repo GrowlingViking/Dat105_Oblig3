055f8c0e825d714d46064c622bdc01872b0d29e6
GrowlingViking <siggesg@hotmail.com>
2017-04-25 11:14:07 +0200

    Added the functions to priorityqueue

--- priorityqueue.h
+++ priorityqueue.h
@@ -1,10 +1,11 @@
 #ifndef PRIORITYQUEUE_H
 #define PRIORITYQUEUE_H
 #include "edge.h"
+#include <vector>
 
 class priorityQueue {
 public:
-    /* Constructor */
+    /* Empty constructor */
     priorityQueue();
 
     /* Destructor */
@@ -18,6 +19,54 @@ public:
 
     /* Checks if the queue is empty */
     bool isEmpty();
+
+    /* Returns the top element in the queue */
+    Edge* top();
+
+    /* Container */
+    vector<Edge> container;
 };
+
+/* Empty constructor */
+priorityQueue::priorityQueue() {
+    vector<Edge> container;
+}
+
+/* Destructor */
+priorityQueue::~priorityQueue() {
+    cout << "Priority Queue is being destroyed!" << endl;
+}
+
+/* Add an Edge object */
+priorityQueue::add(Edge *e) {
+    if (isEmpty()) {
+        container.push_back(e);
+    } else {
+        int i = 0;
+        while (e->weight < container.at(i)->weight) {
+            i ++;
+        }
+        container.insert(i + 1, e);
+    }
+}
+
+/* Remove the edge with the highest priority */
+priorityQueue::remove() {
+    if (isEmpty()) {
+        cout << "The queue is empty" << endl;
+    } else {
+        container.pop_back();
+    }
+}
+
+/* Checks if the queue is empty */
+priorityQueue::isEmpty() {
+    return container.empty();
+}
+
+/* Returns the top element of the queue */
+priorityQueue::top() {
+    return container.back();
+}
 
 #endif // PRIORITYQUEUE_H
